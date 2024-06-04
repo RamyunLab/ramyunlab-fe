920572329d79cdc382498974554dd83ef6aa1828
jjm <jaeminjeon123@gmail.com>
2024-06-04 15:09:59 +0900

    feat: Header 기본형

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import React from "react";
+
+import Header from "./components/Header/Header.tsx";
+
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="App">
+            <header className="App-header">
+                <Header />
+            </header>
+        </div>
+    );
 }
 
 export default App;
