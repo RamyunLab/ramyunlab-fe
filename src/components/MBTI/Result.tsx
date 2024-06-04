378f5bf3d8f30d71ec37368ae9bf8f007ef5a7d1
jjm <jaeminjeon123@gmail.com>
2024-06-04 20:19:33 +0900

    FIX: MBTI 설명 추가

--- src/components/MBTI/Result.tsx
+++ src/components/MBTI/Result.tsx
@@ -1,9 +1,10 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { calculateMBTI } from "../../Redux/slices/MbtiSlice.tsx";
+import { calculateMBTI, resetResult } from "../../Redux/slices/MbtiSlice.tsx";
 import { RootState } from "../../Redux/store";
 import styles from "./Result.module.scss";
 import { Link } from "react-router-dom";
+
 const Result: React.FC = () => {
     const dispatch = useDispatch();
     const answers = useSelector((state: RootState) => state.mbti.answers);
@@ -15,24 +16,24 @@ const Result: React.FC = () => {
         }
     }, [answers, dispatch]);
 
-    // const handleGoHome = () => {
-    //     // 결과 상태를 초기화합니다.
-    //     dispatch(resetResult());
-    // };
     // 결과를 공유하는 함수
     const handleShareResult = () => {
         // 여기에 결과를 공유하는 로직을 구현하세요
+        alert("결과를 공유합니다: " + JSON.stringify(result));
     };
 
-    if (!result) return <div></div>;
+    if (!result) return <div>라면 찾는 중....</div>;
 
     return (
-        <div>
+        <div className={styles.resultPage}>
             <div className={styles.resultContainer}>
-                <h2 className={styles.resultText}>{result}</h2>
+                <h2 className={styles.resultText}>Your MBTI Type: {result.mbtiType}</h2>
+                <p className={styles.ramenRecommendation}>
+                    Ramen Recommendation: {result.ramenRecommendation}
+                </p>
+                <p className={styles.description}>{result.description}</p>
             </div>
-
-            <Link to="/" onClick={() => dispatch(resetResult())}>
+            <Link to="/" onClick={() => dispatch(resetResult())} className={styles.homeLink}>
                 <div>홈으로</div>
             </Link>
             <button className={styles.button} onClick={handleShareResult}>
