fa4627a27d9d8723344c636f9cd16e6414911767
jjm <jaeminjeon123@gmail.com>
2024-06-09 22:36:24 +0900

    feat: 배너 로고

--- src/page/MainPage.tsx
+++ src/page/MainPage.tsx
@@ -1,23 +1,36 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./banner.css"; // Import the CSS file for styling
+import "./banner.scss"; // Import the CSS file for styling
+import mbtiImage from "../assets/images/ramyunmbti.jpg"; // Import the image
+import worldcupImage from "../assets/images/worldcup.jpg"; // Import the worldcup image
 
 const MainPage: React.FC = () => {
     return (
-        <div>
-            <Link to="/mbti" className="banner-link">
-                <div className="banner">Go to MBTI Page</div>
-            </Link>
-            {/* 월드컵 페이지로 가는 새로운 링크를 추가합니다. */}
-            <Link to="/tournament" className="banner-link">
-                <div className="banner">Go to Worldcup Page</div>
-            </Link>
-            <Link to="/ramen/:idx" className="banner-link">
-                <div className="banner">라면 상세페이지</div>
-            </Link>
-            <Link to="/UpDownGame" className="banner-link">
-                <div className="banner">Go to UPDOWNGAME Page</div>
-            </Link>
+        <div className="banners-container">
+            <div className="banner">
+                <img src={mbtiImage} alt="MBTI Banner" />
+                <Link to="/mbti" className="banner-link">
+                    <div className="banner-text"></div>
+                </Link>
+            </div>
+            <div className="banner">
+                <img src={worldcupImage} alt="Worldcup Banner" />
+                <Link to="/tournament" className="banner-link">
+                    <div className="banner-text"></div>
+                </Link>
+            </div>
+            <div className="banner">
+                <img src="/path/to/your/updowngame-image.jpg" alt="UpDownGame Banner" />
+                <Link to="/UpDownGame" className="banner-link">
+                    <div className="banner-text">Go to UPDOWNGAME Page</div>
+                </Link>
+            </div>
+            <div className="banner">
+                <img src="/path/to/your/ramen-detail-image.jpg" alt="Ramen Detail Banner" />
+                <Link to="/ramen/:idx" className="banner-link">
+                    <div className="banner-text">라면 상세페이지</div>
+                </Link>
+            </div>
         </div>
     );
 };
