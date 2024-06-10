21fc7754b3899a402b71b8b150603b948d2dc0ee
eastorigin <eastorigin21@gmail.com>
2024-06-10 14:37:26 +0900

    fix: [FE] 메인페이지에서 토너먼트 모달 띄우기

--- src/page/Tournament.tsx
+++ src/page/Tournament.tsx
@@ -1,35 +1,19 @@
 import React, { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import axios from "axios";
 import Matchup from "../components/Tournament/Matchup.tsx";
 import FinalScreen from "../components/Tournament/FinalScreen.tsx";
-import TournamentModal from "../components/Tournament/TournamentModal.tsx";
-import { useSelector, useDispatch } from "react-redux";
-import { useLocation } from "react-router-dom";
-import {
-    setRound,
-    setCurrentMatchups,
-    addWinner,
-    clearWinners,
-    setChampion,
-    setCurrentMatchIndex,
-    resetTournament,
-} from "../Redux/slices/TournamentSlice.tsx";
-import { RootState } from "../Redux/store";
+import { GameDTO } from "../Redux/types";
 import "./Tournament.scss";
-import "./TournamentModal.scss";
-import { GameDTO } from "../Redux/types.ts";
 
 const Tournament: React.FC = () => {
-    const dispatch = useDispatch();
     const location = useLocation();
-    const { round, currentMatchups, winners, champion, currentMatchIndex } = useSelector(
-        (state: RootState) => state.tournament
-    );
-
-    useEffect(() => {
-        // 페이지가 로드될 때마다 토너먼트 상태를 초기화합니다.
-        dispatch(resetTournament());
-    }, [location, dispatch]);
+    const initialRound = location.state?.rounds || 16;
+    const [round, setRound] = useState<number>(initialRound);
+    const [currentMatchups, setCurrentMatchups] = useState<GameDTO[]>([]);
+    const [winners, setWinners] = useState<GameDTO[]>([]);
+    const [champion, setChampion] = useState<GameDTO | null>(null);
+    const [currentMatchIndex, setCurrentMatchIndex] = useState<number>(0);
 
     useEffect(() => {
         if (round !== null) {
@@ -38,48 +22,43 @@ const Tournament: React.FC = () => {
                 .then((response) => {
                     const data: GameDTO[] = response.data.data;
                     if (Array.isArray(data)) {
-                        dispatch(setCurrentMatchups(data));
+                        setCurrentMatchups(data);
                     } else {
                         console.error("응답 데이터가 배열이 아닙니다:", data);
                     }
-                    dispatch(setCurrentMatchIndex(0));
+                    setCurrentMatchIndex(0);
                 })
                 .catch((error) => {
                     console.error("라면 목록 조회 실패:", error);
                 });
         }
-    }, [round, dispatch]);
+    }, [round]);
 
     const handleWinnerSelect = (winner: GameDTO) => {
-        if (round === null) return;
+        const newWinners = [...winners, winner];
+        setWinners(newWinners);
 
-        dispatch(addWinner(winner));
-
-        if (winners.length + 1 === currentMatchups.length / 2) {
+        if (newWinners.length === currentMatchups.length / 2) {
             if (round === 2) {
-                dispatch(setChampion(winner));
+                setChampion(newWinners[0]);
             } else {
                 setTimeout(() => {
-                    dispatch(setCurrentMatchups(winners.concat(winner)));
-                    dispatch(clearWinners());
-                    dispatch(setRound(round / 2));
-                    dispatch(setCurrentMatchIndex(0));
+                    setCurrentMatchups(newWinners);
+                    setWinners([]);
+                    setRound(round / 2);
+                    setCurrentMatchIndex(0);
                 }, 0);
             }
         } else {
-            dispatch(setCurrentMatchIndex(currentMatchIndex + 1));
+            setCurrentMatchIndex(currentMatchIndex + 1);
         }
     };
 
     const handleTournamentStart = (rounds: number) => {
-        dispatch(setRound(rounds));
-        dispatch(clearWinners());
-        dispatch(setChampion(null));
+        setRound(rounds);
+        setWinners([]);
+        setChampion(null);
     };
-
-    if (round === null) {
-        return <TournamentModal onSelect={handleTournamentStart} />;
-    }
 
     if (champion) {
         return <FinalScreen champion={champion} />;
@@ -87,15 +66,9 @@ const Tournament: React.FC = () => {
 
     const currentMatchup = currentMatchups.slice(currentMatchIndex * 2, currentMatchIndex * 2 + 2);
 
-    const getRoundName = (round: number) => {
-        if (round === 4) return "준결승전";
-        if (round === 2) return "결승전";
-        return `${round}강`;
-    };
-
     return (
         <div className="tournament">
-            <h1>{getRoundName(round)}</h1>
+            <h1>{round > 4 ? `${round}강` : round === 4 ? "준결승전" : "결승전"}</h1>
             {currentMatchup.length === 2 && (
                 <Matchup
                     round={round}
